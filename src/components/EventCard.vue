9dc3e8954fa0a701cdaaff1bb2d90c46d45b544f
Kyaw Zayya <kyawzayya.ucsp@gmail.com>
2019-05-12 22:44:59 -0700

    Add existing project

---
+++ src/components/EventCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>{ title }</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Park Cleanup'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
